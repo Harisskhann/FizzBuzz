281a91be809c79f39cd8d6f88273cc0c7f2e8a7b
M.Haris Khan <hkhan@cedrus.com>
2023-07-21 16:02:54 +0500

    Feat;FizzBuzz

---
+++ FizzzBuzz/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Algorithms
+{
+    public class FizzBuzz
+    {
+        public static void RunFizzBuzz(int number)
+        {
+            for(int i = 1; i <= number; i++)
+            {  }
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (number % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (number % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }
+               
+          
+           
+        }
+        public static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter a Number");
+            num=Convert.ToInt32( Console.ReadLine());
+            RunFizzBuzz(num);
+            Console.ReadLine();
+        }
+
+
+    }
+
+
+}
+   
